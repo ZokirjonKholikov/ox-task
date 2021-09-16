e472418e228c61f4e215e1d86a1d60ca0ae4672f
xoliqov-zokirjon <zokirjonxoliqov97@gmail.com>
2021-09-16 11:20:47 +0500

    Complete

--- src/components/Navbar/TabletDesktop/index.jsx
+++ src/components/Navbar/TabletDesktop/index.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import classNames from 'classnames';
-import { Container, UserInfoContainer, Wrapper } from './style';
+import {
+  Container,
+  UserInfoContainer,
+  Wrapper,
+  DropdownContainer,
+  Item,
+  Text,
+  PopupContainer,
+} from './style';
 import { Image, Content } from '../style';
+import Icon from '../../Icon';
 import { DynamicImage } from '../../FormElements/DynamicImage';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { ClickOutside } from '../../../hooks/click-outside';
+import { logout } from '../../../redux/modules/auth/actions';
 const titleClass = classNames('body-large', 'weight-medium', 'text-black-800');
 export default () => {
   const title = useSelector((state) => state.navReducer.title);
-
+  const clickRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const handleOnClick = (type) => {
+    switch (type) {
+      case 'logout':
+        setIsOpen(false);
+        dispatch(logout());
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <Container className='shadow-primary-4'>
       <Wrapper>
@@ -15,20 +38,46 @@ export default () => {
       </Wrapper>
       <Content>
         <>
-          <UserInfoContainer>
-            <span
-              className={classNames(
-                'weight-regular',
-                'button-large',
-                'text-black-800'
-              )}
+          <PopupContainer>
+            <UserInfoContainer>
+              <span
+                className={classNames(
+                  'weight-regular',
+                  'button-large',
+                  'text-black-800'
+                )}
+              >
+                Hi, Zokirjon
+              </span>
+              <Image>
+                <DynamicImage
+                  name={'Zokirjon'}
+                  onClick={() => setIsOpen((prevState) => !prevState)}
+                />
+              </Image>
+            </UserInfoContainer>
+            <ClickOutside
+              outClickRef={clickRef}
+              outsideClicked={() => {
+                setIsOpen(false);
+              }}
             >
-              Hi, Zokirjon
-            </span>
-            <Image>
-              <DynamicImage name={'Zokirjon'} />
-            </Image>
-          </UserInfoContainer>
+              <DropdownContainer isOpen={isOpen}>
+                <Item onClick={() => handleOnClick('logout')}>
+                  <Icon icon='log-in' color='#262626' size='1.2rem' />
+                  <Text
+                    className={classNames(
+                      'caption',
+                      'weight-semibold',
+                      'text-black-800'
+                    )}
+                  >
+                    Log out
+                  </Text>
+                </Item>
+              </DropdownContainer>
+            </ClickOutside>
+          </PopupContainer>
         </>
       </Content>
     </Container>
